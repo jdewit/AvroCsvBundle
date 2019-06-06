139667be25ae68acca32d54eccde033ac26aec55
Steffen Roßkamp <steffen.rosskamp@gimmickmedia.de>
2019-06-06 16:05:01 +0200

    Fix Travis config for deprecations
    * Re-enable coverage

--- Tests/DependencyInjection/ConfigurationTest.php
+++ Tests/DependencyInjection/ConfigurationTest.php
@@ -1,10 +1,6 @@
 <?php
 
 /*
- * This file is part of the PLEGRO application.
- *
- * (c) AS-Trainer <http://www.as-trainer.com/>
- *
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
  */
@@ -12,12 +8,13 @@
 namespace Avro\CsvBundle\Tests\DependencyInjection;
 
 use Avro\CsvBundle\DependencyInjection\Configuration;
+use PHPUnit\Framework\TestCase;
 use Symfony\Component\Config\Definition\Builder\TreeBuilder;
 
 /**
  * @author Steffen Roßkamp <steffen.rosskamp@gimmickmedia.de>
  */
-class ConfigurationTest extends \PHPUnit_Framework_TestCase
+class ConfigurationTest extends TestCase
 {
     public function testGetConfigTreeBuilder()
     {
